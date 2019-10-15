9e2cedc083af8a179e760dc64a8ec4ba4da2f57e
Matteo Mauro <mauro.matteo96@gmail.com>
2019-10-15 16:26:23 +0200

    added test for IT with Redis

---
+++ src/test/java/RedisTestIT.java
@@ -0,0 +1,40 @@
+
+import static org.hamcrest.core.IsEqual.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.redisson.Redisson;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * Remember to download Redis and instantiate a container
+ * docker run --name redis -p 6379:6379 redis:5.0.6
+ * @author matteo
+ */
+public class RedisTestIT {
+
+    private final String PORT = "6379";
+    private final String URL = "redis://127.0.0.1:";
+
+    @Test
+    public void persist() {
+        // 1. Create config object
+        Config config = new Config();
+        config.useSingleServer().setAddress(URL + PORT);
+        // 2. Create Redisson instance
+        RedissonClient redisson = Redisson.create(config);
+        // 3. Get Redis based Map
+        RMap<Long, String> myMap = redisson.getMap("myMap");
+        myMap.put(1L, "hello");
+        String mapValue = myMap.get(1L);
+        assertThat(mapValue, equalTo("hello"));
+    }
+}
